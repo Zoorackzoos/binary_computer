0fc0688add1783d240eddfeb6028d4736dea63ec
Duncan Holmes <duncancholmes@gmail.com>
2026-02-07 15:01:48 -0600

    readed a shit point for this program. not fair to me

---
+++ src/finished/playground.py
@@ -0,0 +1,8 @@
+from src.decimal_to_binary.eight_bit import *
+from src.decimal_to_binary.sixteen_bit import *
+from src.decimal_to_binary.thirty_two_bit import *
+from src.decimal_to_binary.thirty_two_bit.thirty_two_bit_signed_decimal_to_binary_array import \
+    thirty_two_bit_signed_decimal_to_binary_array_no_print_statements
+
+if __name__ == "__main__":
+    print(thirty_two_bit_signed_decimal_to_binary_array_no_print_statements(decimal=-5))
