7d7b8baa00855faf40fbfb1f1908ba35e42754f7
Duncan Holmes <duncancholmes@gmail.com>
2026-02-24 16:30:39 -0600

    drinking lead rn

--- src/finished/float_lab_help/unknown.py
+++ src/finished/float_lab_help/unknown.py
@@ -8,8 +8,5 @@ from src.finished.A_universal_functions.thirty_two_bit.float.IEEE_754_header_arr
 
 if __name__ == "__main__":
     print("main started")
-    print( thirty_two_bit_unsigned_number_to_binary_array_no_print_statements(number=(0xEC02)) )
-    print( thirty_two_bit_unsigned_number_to_binary_array_no_print_statements(number=(0xEC02 << 3)) )
-    #[0, 0, 0, 0, ' ', 0, 0, 0, 0, ' ', 0, 0, 0, 0, ' ', 0, 1, 1, 1, ' ', 0, 1, 1, 0, ' ', 0, 0, 0, 0, ' ', 0, 0, 0, 1, ' ', 0, 0, 0, 0]
-    print(0b00000000000001110110000000010000)
+    print(0x1400 + (8*48+3)*4 )
     print("main finished")
