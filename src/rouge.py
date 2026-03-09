79310c03fe822fa2391724a526d7c75eb7b220b9
Duncan Holmes <dholmes4@huskers.unl.edu>
2026-03-08 21:48:31 -0500

    rouge.py

--- src/rouge.py
+++ src/rouge.py
@@ -2,6 +2,7 @@ from src.finished.A_universal_functions.thirty_two_bit.thirty_two_bit_value_head
 from src.finished.A_universal_functions.thirty_two_bit.thirty_two_bit_unsigned_decimal_to_binary_array import *
 
 if __name__ == "__main__":
-    chapter_6_quiz_q_2 = 0x0D800000 + (16*17)
-    print( chapter_6_quiz_q_2 )
-    print( hex(0x0D800000 + (16*17)) )
+    #chapter_6_quiz_q_2 = 0x0D800000 + (16*17)
+    #print( chapter_6_quiz_q_2 )
+    #print( hex(0x0D800000 + (16*17)) )
+    print(hex( ( 0x0E000000 ) + (300 * 128) * 512 ) )
